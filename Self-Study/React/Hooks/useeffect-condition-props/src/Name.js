4ff0dbeff4f252f37f333050b2c6902e30ac39bc
PERFICIENT\pallavi.kanfade <pallavi.kanfade@perficient.com>
2022-03-24 08:48:00 +0530

    UseEffect in props

--- Self-Study/React/Hooks/useeffect-condition-props/src/Name.js
+++ Self-Study/React/Hooks/useeffect-condition-props/src/Name.js
@@ -5,7 +5,10 @@ function Name(props){
         console.warn("Updates name" + props.Name)
     },[props.Name])
     return(
+        <>
+        <h2>UseEffect in props</h2>
         <h1>{props.Name}</h1>
+        </>
     );
 }
 
