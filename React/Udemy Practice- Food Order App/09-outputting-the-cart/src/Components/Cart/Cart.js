9692436449de62ae56215d5464af34f41217a335
PERFICIENT\pallavi.kanfade <pkanfade@ford.com>
2023-06-15 18:32:11 +0530

    React udemy Course

--- React/Udemy Practice- Food Order App/09-outputting-the-cart/src/Components/Cart/Cart.js
+++ React/Udemy Practice- Food Order App/09-outputting-the-cart/src/Components/Cart/Cart.js
@@ -7,13 +7,17 @@ import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(Cartcontext);
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const totalAmount = `$${cartCtx.totalAmount(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
-  
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
+
   const cartItems = (
     <ul className='cart-items'>
       {cartCtx.items.map((item) => (
--- React/Udemy Practice- Food Order App/09-outputting-the-cart/src/Components/Cart/CartItem.js
+++ React/Udemy Practice- Food Order App/09-outputting-the-cart/src/Components/Cart/CartItem.js
@@ -1,18 +1,18 @@
 import './CartItem.css';
 
 const CartItem = (props) => {
-  const price = `$${props.price.toFixed(2)}`;
+  const price = `$${props.price(2)}`;
 
   return (
     <li className='cart-item'>
       <div>
         <h2>{props.name}</h2>
-        <div className='summary'>
-          <span className='price'>{price}</span>
+        <div className='summary-cartitem'>
+          <span className='price-cartitem'>{price}</span>
           <span className='amount'>x {props.amount}</span>
         </div>
       </div>
-      <div className='actions'>
+      <div className='actions-cartitem'>
         <button onClick='onRemove'>−</button>
         <button onClick='onAdd'>+</button>
       </div>
