bd0f50639a3bc420933a4a2fc59ebb0efa29164d
DESKTOP-BB05MJB\pallavi <pallavi.perficient@gmail.com>
2024-05-05 13:32:44 +0530

    update-app-file

--- React/Udemy Practice- Expense Tracker/01-starting-setup/src/App.js
+++ React/Udemy Practice- Expense Tracker/01-starting-setup/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   ];
   return (
     <div>
-      <p>Exprense tracker</p>
+      <p>Exprense tracker -updates</p>
       <ExpenseItem  expensedata={expenses}/>
     </div>
   );
